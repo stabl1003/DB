e9574a0d943c213f62b95e101f67de42e7dfcb79
stabl <stabl.j9@gmail.com>
2025-11-16 21:26:29 +0100

    another part of binary tree

--- tree/tree.c
+++ tree/tree.c
@@ -1,5 +1,7 @@
 /* tree.c */
 #include "tree.h"
+#include <assert.h>
+#include <stdlib.h>
 
 Tree root = { .n = {
     .tag = (TagRoot | TagNode),
@@ -8,6 +10,29 @@ Tree root = { .n = {
     .right = 0,
     .path = "/"  
 }};
+
+void zero(int8 *str, int16 size) {
+    int8 *p;
+    int16 n;
+
+    for (n=0, p=str;n < size; p++, n++)
+        *p = 0;
+      
+    return;
+};
+
+Node *create_node(Node *parent, int8 *path) {
+     Node *n;
+     int16 size;
+
+     assert(parent);
+     size = sizeof(struct s_node);
+     n = (Node *)malloc(size);
+     zero((int8 *)n, size);
+
+     parent->right = n;
+     
+}
 
 int main() {
   
