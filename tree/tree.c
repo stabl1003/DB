784dd632c6d8472e6848164b255632250858fa53
stabl <stabl.j9@gmail.com>
2025-11-16 23:49:50 +0100

    2 fucking errors

--- tree/tree.c
+++ tree/tree.c
@@ -1,7 +1,9 @@
 /* tree.c */
 #include "tree.h"
 #include <assert.h>
+#include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 Tree root = { .n = {
     .tag = (TagRoot | TagNode),
@@ -27,16 +29,28 @@ Node *create_node(Node *parent, int8 *path) {
 
      assert(parent);
      size = sizeof(struct s_node);
-     n = (Node *)malloc(size);
+     n = (Node *)malloc((int)size);
      zero((int8 *)n, size);
 
      parent->right = n;
-     
+     n->tag = TagNode;
+     n->up = parent;
+     strncpy((char *)n->path, (char *)path, 255);
+
+     return n;
 }
 
 int main() {
+  Node *n, *n2;
+
+  n = create_node((Node *)&root,(int8 *)"/Users");
+  assert(n);
   
-  printf("%p\n", (void *)&root);
-  
+  n2 = create_node(n,(int8 *)"/Users/login");
+  assert(n2);
+
+  printf("%p %p\n", n, n2);
+  free(n2);
+  free(n);
   return 0;
 }
