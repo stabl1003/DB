626461fe7483ca7bb587cdfc0d9058207d312c5d
stabl <stabl.j9@gmail.com>
2025-11-19 22:19:48 +0100

    update on search

--- tree/tree.c
+++ tree/tree.c
@@ -1,5 +1,8 @@
 /* tree.c */
 #include "tree.h"
+#include <assert.h>
+#include <stdio.h>
+#include <string.h>
 
 Tree root = { .n = {
     .tag = (TagRoot | TagNode),
@@ -52,9 +55,8 @@ Leaf *find_last_linear(Node *parent) {
    return l;
 }
 
-Leaf *create_leaf(Node *parent, int8 *key, int8 value, int16 count) {
+Leaf *create_leaf(Node *parent, int8 *key, int8 *value, int16 count) {
     Leaf *l, *new;
-    Node *n;
     int16 size;
 
     assert(parent);
@@ -87,12 +89,25 @@ Leaf *create_leaf(Node *parent, int8 *key, int8 value, int16 count) {
 
 int main() {
   Node *n, *n2;
+  Leaf *l1, *l2;
+  int8 *key, *value;
+  int16 *size;
 
   n = create_node((Node *)&root,(int8 *)"/Users");
   assert(n);
   
   n2 = create_node(n,(int8 *)"/Users/login");
   assert(n2);
+  
+  key = (int8 *)"stabl";
+  value = (int8 *)"abc77301aa";
+  size = (int16 *)strlen((char *)value);
+  
+  l1 = create_leaf(n2, key, value,(int16 )size);
+  assert(l1);
+
+  printf("%s\n", l1->value);
+  
 
   printf("%p %p\n", n, n2);
   free(n2);
