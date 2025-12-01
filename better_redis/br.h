dbe09642cfa7ce1dd365ff927a152ebb2804e444
stabl <stabl.j9@gmail.com>
2025-12-01 13:17:59 +0100

    some updates

---
+++ better_redis/br.h
@@ -0,0 +1,26 @@
+/* br.h */
+#ifndef CACHE22
+#define CACHE22
+
+#define _GNU_SOURCE
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <stdbool.h>
+#include <assert.h>
+#include <errno.h>
+
+#include <arpa/inet.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+
+#define HOST      "127.0.0.1"
+#define PORT      12000   
+
+typedef unsigned int int32;
+typedef unsigned short int int16;
+typedef unsigned char int8;
+
+void mainloop(void);
+int main(int, char**);
--- better_redis/main.c
+++ better_redis/main.c
@@ -1,7 +1,23 @@
 /* main.c */
 #include "br.h"
-#include <stdio.h>
+
+bool scontiunation;
+
+void mainloop() {
+    struct sockaddr_in sock;
+    int s;
+
+    sock.sin_family = AF_INET;
+    sock.sin_port = htons(PORT);
+    sock.sin_addr.s_addr = inet_addr(HOST);
+
+    s = socket(AF_INET, SOCK_STREAM, 0);
+}
 
 int main(int argc, char *argv[]){
-  printf("ok\n");
+    scontiunation = true;
+    while(scontiunation)
+        mainloop();
+
+    return 0;
 }
